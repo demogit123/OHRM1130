c4c6c42036c4de28067be648ad7764249dfc6173
prave <prave@DESKTOP-5JMI9DE>
2022-10-22 00:03:20 +0530

    test case 3

---
+++ MyProject/src/testcases/TeseCase3.java
@@ -0,0 +1,13 @@
+package testcases;
+
+public class TeseCase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is test case 3");
+		
+		
+	}
+
+}
